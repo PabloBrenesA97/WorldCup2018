021257026db5921df0b6ddb278e8da662f4c44f8
jdamador <amadorsalasjd@gmail.com>
2017-11-05 18:01:43 -0600

    method to create default stadiums

--- WorldChampionCup/src/pk/codeapp/controller/AppController.java
+++ WorldChampionCup/src/pk/codeapp/controller/AppController.java
@@ -6,18 +6,38 @@
 package pk.codeapp.controller;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
 import pk.codeapp.model.Stadium;
-=======
 import pk.codeapp.model.Team;
->>>>>>> master
 
 /**
  *
  * @author Daniel Amador
  */
 public class AppController {
+
+    private Maker maker = new MasterMaker();
+
     private ArrayList<Team> teams = new ArrayList(); // List of Teams
+    private ArrayList<Stadium> arrayStadiums = new ArrayList();
+
+    public void createDefaultStadiums() {
+        String[] stadiumsName
+                = {"Kaliningrad Stadium", "Saint Petersburg Stadium", "Luzhniki Stadium",
+                    "Spartak Stadium", "Kazan Arena", "Nizhny Novgorod Stadium", "Samara Arena",
+                    "Mordovia Arena", "Volgograd Arena", "Rostov Arena", "Fisht Stadium",
+                    "Ekaterinburg Arena"};
+        int[] stadiumsCapacity = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int[] capacityStadium = {45015, 68134, 81500, 45360, 45379, 44899, 63000, 45100, 45568, 43702, 47659};
+        String[] cityStadiums = {"Kalingrand", "Saint Petersburg", "Moscow", "Moscow",
+            "Kazan", "Nizhny Novgorod", "Samara", "Saransk", "Volgograd", "Rostov-on-Don",
+            "Sochi", "Ekateringburg"};
+        for (int i = 0; i < stadiumsName.length; i++) {
+            Stadium newStadium = (Stadium) maker.factoryMethod("Stadium");
+            newStadium.update(stadiumsName[i], capacityStadium[i], cityStadiums[i], capacityStadium[i]);
+            arrayStadiums.add(newStadium);
+        }
+    }
+    //<editor-fold desc="All Getter & Setter here" defaultstate="collapsed">
 
     public ArrayList<Team> getTeams() {
         return teams;
@@ -28,11 +48,21 @@ public class AppController {
     }
     
     
-    
-    ArrayList<Stadium> ArrayStadiums = new ArrayList();
-    
-    
-    public void CreateDefaultStadiums(){
-        String[] stadiumsName= {};
+
+    public Maker getMaker() {
+        return maker;
     }
+
+    public void setMaker(Maker maker) {
+        this.maker = maker;
+    }
+
+    public ArrayList<Stadium> getArrayStadiums() {
+        return arrayStadiums;
+    }
+
+    public void setArrayStadiums(ArrayList<Stadium> arrayStadiums) {
+        this.arrayStadiums = arrayStadiums;
+    }
+    //</editor-fold>
 }
