8ddcb33640d482608a2b63012293930faa10d929
jdamador <amadorsalasjd@gmail.com>
2017-11-15 23:27:26 -0600

    show matches

--- WorldChampionCup/src/pk/codeapp/controller/ShowStadiumController.java
+++ WorldChampionCup/src/pk/codeapp/controller/ShowStadiumController.java
@@ -7,6 +7,7 @@ package pk.codeapp.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.DefaultListModel;
 import pk.codeapp.model.Stadium;
 import pk.codeapp.view.ShowStadium;
 
@@ -31,9 +32,20 @@ public class ShowStadiumController implements ActionListener {
         window.lblId.setText("Id: "+stadium.getId());
         window.lblCity.setText("City: " +stadium.getCity());
         window.lblName.setText("Name: "+stadium.getName());
+        chargeCalendars();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    }
+
+    private void chargeCalendars() {
+        DefaultListModel<String> model = new DefaultListModel();
+        
+        for (int i = 0; i < stadium.getMatches().size(); i++) {
+            model.addElement(stadium.getMatches().get(i).getTeam1().getName()+ " vs " +stadium.getMatches().get(i).getTeam2().getName()+ " Date: "+ stadium.getMatches().get(i).getDate() );
+            model.addElement("");
+        }
+        window.jList1.setModel(model);
     }
 }
